33bf83f857a197f92f7ef7bb49baa872ec8873b8
Peter Carl Pardo <peterpardo123@gmail.com>
2022-06-07 22:30:58 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import bgPattern from "./assets/pattern-bg.png";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="h-screen">
+      {/* bg pattern */}
+      <div
+        className="bg-no-repeat bg-local h-56"
+        style={{ backgroundImage: `url(${bgPattern})` }}>
+        {/* Title */}
+        <h1 className="text-center text-white text-2xl font-medium py-5">
+          IP Address Tracker
+        </h1>
+
+        {/* search bar */}
+        <div className="flex justify-center mb-4 px-7">
+          <input
+            className="px-3 py-2 rounded-l-lg outline-none w-full max-w-sm"
+            type="text"
+            value="192.168.1.1"
+          />
+          <button
+            className="bg-dark-gray-dark py-2 px-5 rounded-r-lg hover:bg-dark-gray"
+            type="button">
+            <svg xmlns="http://www.w3.org/2000/svg" width="11" height="14">
+              <path
+                fill="none"
+                stroke="#FFF"
+                strokeWidth="3"
+                d="M2 1l6 6-6 6"
+              />
+            </svg>
+          </button>
+        </div>
+
+        {/* result */}
+        <div>
+          <div>
+            <span>IP ADDRESS</span>
+            <span>192.168.1.1</span>
+          </div>
+          <div>
+            <span>LOCATION</span>
+            <span>Brooklyn, NY 10001</span>
+          </div>
+          <div>
+            <span>TIMEZONE</span>
+            <span>UTC-05:00</span>
+          </div>
+          <div>
+            <span>ISP</span>
+            <span>SpaceX Starlink</span>
+          </div>
+        </div>
+      </div>
+
+      {/* map */}
+    </main>
   );
-}
+};
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "dark-gray": {
+          DEFAULT: "#969696",
+          dark: "#2b2b2b",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
